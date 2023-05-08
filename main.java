c2bbec825a492151e83b9a4a773f71b7c05ee84c
AlexaDickenson <127786567+AlexaDickenson@users.noreply.github.com>
2023-05-08 19:25:55 -0400

    Create main.java

---
+++ main.java
@@ -0,0 +1,21 @@
+class MyNameIs {
+    public static void main(String[] args) {
+        System.out.println("My name is ");
+        System.out.println("Alexa");
+        System.out.println("Dickenson");
+    }
+}
+
+class Vacation{
+    public static void main(String[]args){
+        System.out.println("I'd like to visit");
+        System.out.println("Paris,");
+        System.out.println("France");
+    }
+}
+class HiFriend{
+    public static void main(String[]args){
+        String s="Hi" +"Larry"+" Sprinkle!";
+        System.out.println(s);//Hi Larry Sprinkle!
+    }
+}
